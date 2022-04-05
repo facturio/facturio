e144725e97e3e5ddd65025978a2bbb5ae5f4350f
Plex <thinkplex@riseup.net>
2022-04-05 16:52:04 +0200

    _str_

--- py_pdf/Info_Facture_Devis.py
+++ py_pdf/Info_Facture_Devis.py
@@ -1,108 +1,160 @@
-
-
 class Utilisateur:
     """
     Classe contenant toutes les informations liées à l'utilisateur
     """
-    def __init__(self, nom_entr=None, email=None, adr=None, tel=None, num_siren=None, logo=None) :
+
+    def __init__(
+        self,
+        nom_entr: str = None,
+        email: str = None,
+        adr: str = None,
+        tel: str = None,
+        num_siren: str = None,
+        logo=None,
+    ):
         """
         Les attributs sont initialisés selon leur ordre d'apparition
         """
-        self.logo = logo                    #image
-        self.nom_entr = nom_entr            #string
-        self.email = email                  #string
-        self.adr = adr                      #string
-        self.tel = tel                      #string
-        self.num_siren = num_siren          #string
+        self.logo = logo  # image
+        self.nom_entr = nom_entr  # string
+        self.email = email  # string
+        self.adr = adr  # string
+        self.tel = tel  # string
+        self.num_siren = num_siren  # string
+
+    def __str__(self):
+        return self.nom_entr + " | " + self.email + " | " + self.tel
 
 
 class Particulier:
     """
     Classe contenant toutes les informations liées à un client physique
     """
-    def __init__(self, nom=None, prenom=None, email=None, adr=None, tel=None):
+
+    def __init__(
+        self,
+        nom: str = None,
+        prenom: str = None,
+        email: str = None,
+        adr: str = None,
+        tel: str = None,
+    ):
         """
         Les attributs sont initialisés selon leur ordre d'apparition
         dans le pdf
         attributs : chaîne de caractères
         """
-        self.nom = nom                      #string
-        self.prenom = prenom                #string
-        self.email = email                  #string
-        self.adr = adr                      #string
-        self.tel = tel                      #string
+        self.nom = nom  # string
+        self.prenom = prenom  # string
+        self.email = email  # string
+        self.adr = adr  # string
+        self.tel = tel  # string
+
+    def __str__(self):
+        return self.nom + " " + self.prenom + " | " + self.email + " | " + self.tel
 
 
 class Entreprise(Particulier):
     """
     Classe contenant toutes les informations liées à un client moral
     """
-    def __init__(self, nom_entr=None, nom=None, prenom=None, email=None, adr=None, tel=None, num_siren=None,):
+
+    def __init__(
+        self,
+        nom_entr: str = None,
+        nom: str = None,
+        prenom: str = None,
+        email: str = None,
+        adr: str = None,
+        tel: str = None,
+        num_siren: str = None,
+    ):
         """
         Les attributs sont initialisés selon leur ordre d'apparition
         dans le pdf
         """
-        self.nom_entr = nom_entr                        #string
-        super().__init__(nom, prenom, adr, tel, email)  #string, string, string, string, string
-        self.num_siren = num_siren                      #string
+        self.nom_entr = nom_entr  # string
+        super().__init__(
+            nom, prenom, adr, tel, email
+        )  # string, string, string, string, string
+        self.num_siren = num_siren  # string
 
 
-class Article():
+class Article:
     """
     Classe contenant toutes les informations liées à un article
     """
-    def __init__(self, nom_article, prix, description):
-        self.nom_article = nom_article                  #string
-        self.prix = prix                                #flottant
-        self.description = description                  #string
+
+    def __init__(self, nom_article: str, prix: float, description: str):
+        self.nom_article = nom_article  # string
+        self.prix = prix  # flottant
+        self.description = description  # string
+
+    def __str__(self):
+        return self.nom_article + " | " + str(self.prix)
 
 
-class Acompte():
+class Acompte:
     """
     Classe contenant toutes les informations liées à un acomptes
     """
-    def __init__(self, date, montant):
-        
-        self.date = date                               #string
-        self.montant = montant                         #flottant
 
-class F_D():
+    def __init__(self, date, montant):
+
+        self.date = date  # string
+        self.montant = montant  # flottant
+
+
+class F_D:
     """
     Classe contenant toutes les informations communes liées aux factures et devis
     """
-    def __init__(self, utilisateur, client, date, liste_articles, montant = None,commentaire=None):
-        
-        self.utilisateur = utilisateur                  #instance de classe utilisateur
-        self.client = client                            #instance de classe client physique ou moral
-        self.date = date                                #string
-        self.liste_articles = liste_articles            #liste de tuples avec comme couple instance classe article et la quantité en int
-        self.montant = montant                          #flottant
-        self.commentaire = commentaire                  #entier
+
+    def __init__(
+        self,
+        utilisateur: Utilisateur,
+        client: Particulier,
+        date: str,
+        liste_articles: [(Article, int)],
+        montant=None,
+        commentaire=None,
+    ):
+        self.utilisateur = utilisateur  # instance de classe utilisateur
+        self.client = client  # instance de classe client physique ou moral
+        self.date = date  # string
+        self.liste_articles = liste_articles  # liste de tuples (instance classe article, quantité en int)
+        self.montant = montant  # flottant
+        self.commentaire = commentaire  # entier
 
 
-class Facture():
-
-    def __init__(self, utilisateur, client, date, liste_articles, liste_acomptes, montant = None,commentaire=None):
+class Facture:
+    def __init__(
+        self,
+        utilisateur,
+        client,
+        date,
+        liste_articles,
+        liste_acomptes,
+        montant=None,
+        commentaire=None,
+    ):
         """
         Classe contenant toutes les informations communes liées aux factures et devis
         """
-        super().__init__(utilisateur, client, date, liste_articles, montant,commentaire) #attribut classe F_D
-        self.liste_acomptes = liste_acomptes             #liste d'instances de classes d'acomptes
+        super().__init__(
+            utilisateur, client, date, liste_articles, montant, commentaire
+        )  # attribut classe F_D
+        self.liste_acomptes = liste_acomptes  # liste d'instances de classes d'acomptes
 
 
-class Devis():
-
-    def __init__(self, utilisateur, client, date, liste_articles, montant = None,commentaire=None):
+class Devis:
+    def __init__(
+        self, utilisateur, client, date, liste_articles, montant=None, commentaire=None
+    ):
         """
-        Classe contenant toutes les informations communes liées aux factures et devis
+        Classe contenant toutes les informations communes liées aux factures et
+        devis.
         """
-        super().__init__(utilisateur, client, date, liste_articles, montant,commentaire) #attribut classe F_D
-        
-
-
-
-
-        
-
-
-       
+        super().__init__(
+            utilisateur, client, date, liste_articles, montant, commentaire
+        )  # attribut classe F_D
---
+++ py_pdf/autocompletion.py
@@ -0,0 +1,41 @@
+"""Module d'autocomplétion."""
+from Info_Facture_Devis import Particulier, Article
+import argparse
+import re
+
+clement = Particulier("Bazan", "Clement", "clement.bazan@email.com",
+                      "AAAAAAAAAAAAAAAAAA", "0123456789")
+quentin = Particulier("Lombardo", "Quentin", "quentin.lombardo@email.com",
+                      "AAAAAAAAAAAAAAAAAAAAAA", "0000000000")
+youssef = Particulier("Benjelloun", "Youssef",
+                      "youssef.benjelloun@email.com",
+                      "AAAAAAAAAAAAAAAAAAAAA", "0101010101")
+
+ordinateur = Article("ordinateur", 1684.33, "Un ordinateur portable.")
+cable_ethernet = Article("cable ethernet", 9.99, "Un câble ethernet.")
+telephone = Article("telephone", 399.99, "Un téléphone.")
+casque = Article("casque", 69.99, "Un casque audio.")
+
+clients = [clement, quentin, youssef]
+articles = [ordinateur, cable_ethernet, telephone, casque]
+
+parser = argparse.ArgumentParser(description="Autocompletion test.")
+parser.add_argument('text', type=str, help="text to autocomplete")
+args = parser.parse_args()
+
+args.text
+
+req = re.compile(".*" + args.text + ".*", re.IGNORECASE)
+cls = []
+art = []
+
+for client in clients:
+    if any([req.match(el) for el in vars(client).values()]):
+        cls.append(client)
+
+for article in articles:
+    if any([req.match(str(el)) for el in vars(article).values()]):
+        art.append(article)
+
+print([str(c) for c in cls])
+print([str(c) for c in art])
