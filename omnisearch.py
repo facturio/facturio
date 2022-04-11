74f635d3adf141b0ef69cc2c3cc6fc0353f1d6ce
Plex <thinkplex@riseup.net>
2022-04-11 14:08:41 +0200

    merge

--- omnisearch.py
+++ omnisearch.py
@@ -1,10 +1,26 @@
 """Module d'autocomplétion."""
 from Info_Facture_Devis import clients, articles
 import re
-
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk  # noqa: E402
+
+# clement = Client("Bazan", "Clement", "clement.bazan@email.com",
+#                       "AAAAAAAAAAAAAAAAAA", "0123456789")
+# quentin = Client("Lombardo", "Quentin", "quentin.lombardo@email.com",
+#                       "AAAAAAAAAAAAAAAAAAAAAA", "0000000000")
+# youssef = Client("Benjelloun", "Youssef",
+#                       "youssef.benjelloun@email.com",
+#                       "AAAAAAAAAAAAAAAAAAAAA", "0101010101")
+
+# ordinateur = Article("ordinateur", 1684.33, "Un ordinateur portable.")
+# cable_ethernet = Article("cable ethernet", 9.99, "Un câble ethernet.")
+# telephone = Article("telephone", 399.99, "Un téléphone.")
+# casque = Article("casque", 69.99, "Un casque audio.")
+
+# clients = [clement, quentin, youssef]
+# articles = [ordinateur, cable_ethernet, telephone, casque]
+
 
 
 class ItemRow(Gtk.ListBoxRow):
