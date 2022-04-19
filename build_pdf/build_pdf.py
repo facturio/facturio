d325c1c73ee49eb07139b5973b1fac7dc4aae065
BozoDozo <clem.boz@hotmail.com>
2022-04-19 15:32:43 +0200

    ajout de disposition et des couleurs

--- build_pdf/build_pdf.py
+++ build_pdf/build_pdf.py
@@ -24,7 +24,7 @@ from pathlib import Path
 
 import sys
 
-from numpy import isin
+
 #Import classes objet
 sys.path.append('../')
 from classes.estimate_invoice import *
@@ -64,6 +64,80 @@ person_icon: LayoutElement = Image(
     padding_top=Decimal(5),
     padding_left=Decimal(3)
 )
+
+dot_icon: LayoutElement = Image(
+    image=Path("icons/dot.png"),
+    width=Decimal(3),
+    height=Decimal(3),
+    padding_top=Decimal(5),
+    padding_left=Decimal(3)
+)
+
+
+small_phone_icon: LayoutElement = Image(
+    image=Path("icons/phone.png"),
+    width=Decimal(7),
+    height=Decimal(7),
+    padding_top=Decimal(3),
+    padding_left=Decimal(3)
+)
+
+small_mail_icon: LayoutElement = Image(
+    image=Path("icons/mail.png"),
+    width=Decimal(7),
+    height=Decimal(7),
+    padding_top=Decimal(3),
+    padding_left=Decimal(3)
+)
+small_email_icon: LayoutElement = Image(
+    image=Path("icons/email.png"),
+    width=Decimal(7),
+    height=Decimal(7),
+    padding_top=Decimal(3),
+    padding_left=Decimal(3)
+)
+
+small_person_icon: LayoutElement = Image(
+    image=Path("icons/person.png"),
+    width=Decimal(7),
+    height=Decimal(7),
+    padding_top=Decimal(3),
+    padding_left=Decimal(3)
+)
+
+small_business_icon: LayoutElement = Image(
+    image=Path("icons/business.png"),
+    width=Decimal(7),
+    height=Decimal(7),
+    padding_top=Decimal(3),
+    padding_left=Decimal(3)
+)
+
+#Fonction pour les couleurs
+def hex_to_rgb(value: str):
+    """Return (red, green, blue) for the color given as #rrggbb."""
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def rgb_to_hex(red: int, green: int, blue: int):
+    """Return color as #rrggbb for the given color values."""
+    return '#%02x%02x%02x' % (red, green, blue)
+
+def shade_color(color: tuple[int, int, int], factor:int):
+        """
+        Retourne la couleur rgb assombri ou éclaircir
+        """
+        shaded_color= []
+        for c in color:
+            x = int(c * factor)
+            if(x > 255):
+                x = 255
+            if(x < 0):
+                x = 0
+            shaded_color.append(x)
+        return shaded_color
+
 def pdf_header(receipt: Union[Invoice, Estimate], id: int):
     """
     Construction de l'entête du pdf
@@ -122,22 +196,28 @@ def pdf_provider_client(receipt: Union[Estimate,Invoice]):
                                         horizontal_alignment=Alignment.RIGHT))
     table.add(Paragraph(" "))
          
+    #On met les champs client, et utilisateur dans des listes
+    #On fait cela car certains champs sont optionnels en utilisant
+    #une liste on évite d'avoir des trous aux endroits où un champ
+    #optionnel n'a pas été rempli
     provider_list = provider.dump_to_field()
     client_list = client.dump_to_field()
     provider_icon_list = [business_icon, email_icon, mail_icon, phone_icon, 
                    person_icon]
-     
+    #Numero SIREN de l'artisan
     buisness_number_table.add(Paragraph("N°SIREN", font="Helvetica-Bold", 
                                     horizontal_alignment=Alignment.LEFT))
     buisness_number_table.add(Paragraph(provider_list[-1], 
                                         horizontal_alignment=Alignment.RIGHT))
     buisness_number_table.add(Paragraph(" ", padding_left=Decimal(145)))
-
+    
+    #Si le champ email pour l'utilisateur n'est pas remplit,
+    #On supprime l'élément dans la liste
     if(provider_list[1] == None):
         del provider_icon_list[1]
         del provider_list[1]
-    
-
+    #On appelle une fonction si le client est une entité morale
+    #ou physique
     if(isinstance(client, Company)):
         table, buisness_number_table = provider_company_table(table, 
         buisness_number_table, provider_list, provider_icon_list, client_list)  
@@ -162,6 +242,10 @@ def provider_company_table(
         provider_icon_list: list, 
         client_list: list
                             ):  
+        """
+        Met en place le layout correspondant aux champs de l'artisan
+        et aux champs du client lorsqu'il est moral
+        """
         company_icon_list = [business_icon, email_icon, mail_icon, phone_icon, 
                    person_icon]
         length_min = len(provider_icon_list)
@@ -172,14 +256,15 @@ def provider_company_table(
             table.add(company_icon_list[i])  
             table.add(Paragraph(client_list[i]))
             i+=1
-
+    #Un bloc if dans l'éventualité ou un utilisateur n'a pas d'email
         if(i != 5):
             table.add(Paragraph(" "))
             table.add(Paragraph(" "))
             table.add(company_icon_list[-1])
             table.add(Paragraph(client_list[-2]))
         
-
+        #Comme toutes les colones d'un tableu doivent être de même
+        #taille on fait un 2ème tableu pour le numero SIREN
         buisness_number_table.add(Paragraph("N°SIREN", font="Helvetica-Bold", 
                                     horizontal_alignment=Alignment.LEFT))
         buisness_number_table.add(Paragraph(client_list[-1])) 
@@ -193,15 +278,21 @@ def provider_individual_table(
         provider_list: list,
         provider_icon_list: list, 
         client_list: list
-                            ):   
+                            ):
+        """
+        Met en place le layout correspondant aux champs de l'artisan
+        et aux champs du client lorsqu'il est physique
+        """   
         client_icon_list = [person_icon, email_icon, mail_icon, phone_icon]
         for i in range(3, 0, -1):
-            
+            #On supprime tous les champs optionnels qui n'ont pas été
+            #remplit
             if(client_list[i] == None):
                 del client_icon_list[i]
                 del client_list[i]
         i = 0
         length_min = len(client_icon_list)
+        
         while(i < length_min):
             table.add(provider_icon_list[i])  
             table.add(Paragraph(provider_list[i]))
@@ -211,25 +302,153 @@ def provider_individual_table(
 
         i = length_min
         length_min = len(provider_icon_list)
+        #Comme l'utilisateur a plus de champs que clients alors on
+        #fait une autre boucle a part
         while(i < length_min):
             table.add(provider_icon_list[i])  
             table.add(Paragraph(provider_list[i]))
             table.add(Paragraph(" "))  
             table.add(Paragraph(" "))
             i+=1
-
+        #Si l'utilisateur n'a pas d'email alors on fait une ligne vide
         if(i != 5):
             table.add(Paragraph(" "))
             table.add(Paragraph(" "))
             table.add(Paragraph(" "))
             table.add(Paragraph(" "))
 
+        #Un cleint physique n'a pas de numero SIREN
         buisness_number_table.add(Paragraph(" "))
         buisness_number_table.add(Paragraph(" "))     
 
         return table, buisness_number_table
-        
-def pdf_articles_total(receipt: Union[Invoice, Estimate], currency: str):
+
+def pdf_provider_inline(receipt: Union[Invoice, Estimate]):
+    """
+    Retourne une table avec les informations liées à l'utilisateur
+    en ligne
+    """
+    artisan = receipt.user
+    if(artisan.email == None):
+        table_01 = FlexibleColumnWidthTable(number_of_rows=1, 
+                                                        number_of_columns=14)
+    else:
+        table_01 = FlexibleColumnWidthTable(number_of_rows=1, 
+                                                        number_of_columns=17)
+    
+
+    table_01.add(small_business_icon)
+    table_01.add(Paragraph(artisan.company_name, font_size=Decimal(8)))
+    table_01.add(dot_icon)
+
+    if(artisan.email):
+        table_01.add(small_email_icon)
+        table_01.add(Paragraph(artisan.email, font_size=Decimal(8)))
+        table_01.add(dot_icon)
+    
+    table_01.add(small_mail_icon)
+    table_01.add(Paragraph(artisan.adr, font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(small_phone_icon)
+    table_01.add(Paragraph(artisan.phone, font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(small_person_icon)
+    table_01.add(Paragraph(f"{artisan.first_name} {artisan.last_name}", 
+                                                        font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(Paragraph("N°SIREN", font="Helvetica-Bold",
+                    font_size=Decimal(8),horizontal_alignment=Alignment.LEFT))
+    table_01.add(Paragraph(artisan.buisness_number, font_size=Decimal(8)))
+
+
+    table_01.set_padding_on_all_cells(Decimal(1), Decimal(1), Decimal(1), 
+                                                                Decimal(1))    		
+    table_01.no_borders()
+
+    return table_01
+
+
+def pdf_company_inline(receipt: Union[Invoice, Estimate]):
+    """
+    Retourne une table avec les informations liées à un client moral
+    en ligne
+    """
+    client = receipt.client
+    
+    table_01 = FlexibleColumnWidthTable(number_of_rows=1,  number_of_columns=17)
+    table_01.add(small_business_icon)
+    table_01.add(Paragraph(client.company_name, font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(small_email_icon)
+    table_01.add(Paragraph(client.email, font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(small_mail_icon)
+    table_01.add(Paragraph(client.adr, font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(small_phone_icon)
+    table_01.add(Paragraph(client.phone, font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(small_person_icon)
+    table_01.add(Paragraph(f"{client.first_name} {client.last_name}", 
+                                                        font_size=Decimal(8)))
+    table_01.add(dot_icon)
+    table_01.add(Paragraph("N°SIREN", font="Helvetica-Bold",
+                    font_size=Decimal(8),horizontal_alignment=Alignment.LEFT))
+    table_01.add(Paragraph(client.buisness_number, font_size=Decimal(8)))
+    table_01.set_padding_on_all_cells(Decimal(1), Decimal(1), Decimal(1), 
+                                                                Decimal(1))    		
+    table_01.no_borders()
+
+    return table_01
+
+
+def pdf_individual_inline(receipt: Union[Invoice, Estimate]):
+    """
+    Retourne une table avec les informations liées à un client moral
+    en ligne
+    """
+    client = receipt.client
+    client_list = client.dump_to_field()[1:]
+    client_icon_list = [small_email_icon, small_mail_icon, small_phone_icon]
+    #On supprime tous les champs optionnels qui n'ont pas été
+    #remplit
+    for i in range(2, -1, -1):
+        if(client_list[i] == None):
+            del client_icon_list[i]
+            del client_list[i]
+
+    n = len(client_list)
+    column_numbers = 2 + 3 * n
+    table_01 = FlexibleColumnWidthTable(number_of_rows=1,  
+                                             number_of_columns=column_numbers)
+    table_01.add(small_person_icon)
+    table_01.add(Paragraph(f"{client.first_name} {client.last_name}", 
+                                                        font_size=Decimal(8)))
+    i = 0
+    while(i < n):
+        table_01.add(dot_icon)   
+        table_01.add(client_icon_list[i])
+        table_01.add(Paragraph(client_list[i], font_size=Decimal(8)))
+        i+=1
+
+    table_01.set_padding_on_all_cells(Decimal(1), Decimal(1), Decimal(1), 
+                                                                Decimal(1))    		
+    table_01.no_borders()
+
+    return table_01
+
+def pdf_client_inline(receipt: Union[Invoice, Estimate]):
+    """
+    Retourne une table avec les informations liées à un client
+    """
+    if(isinstance(receipt.client, Company)):
+        return pdf_company_inline(receipt)
+
+    elif(isinstance(receipt.client, Client)):
+        return pdf_individual_inline(receipt)
+    
+def pdf_articles_total(receipt: Union[Invoice, Estimate], currency: str, 
+                                                        color: str):
     """
     Construit la listes des articles et des taxes
     """
@@ -247,11 +466,11 @@ def pdf_articles_total(receipt: Union[Invoice, Estimate], currency: str):
         table.add(  
             TableCell(  
                 Paragraph(h, font_color = HexColor("ffffff")),  
-                background_color = HexColor("5f5f5f"),  
+                background_color = HexColor(color),  
             )  
         ) 
     #Couleur différente pour chaque lignes qui se suivent 
-    odd = HexColor("BBBBBB")  
+    odd = HexColor(rgb_to_hex(*shade_color(hex_to_rgb(color),1.6)))  
     even = HexColor("FFFFFF")  
     #Variable pour savoir la parité de la ligne
     parity = True
@@ -288,7 +507,7 @@ def pdf_articles_total(receipt: Union[Invoice, Estimate], currency: str):
         table = pdf_invoice_total(table, receipt, currency) 
     elif(isinstance(receipt, Estimate)):
         table = pdf_estimate_total(table, receipt, currency) 
-    
+
     table.set_padding_on_all_cells(Decimal(2), 
                                         Decimal(15), Decimal(2), Decimal(2))
     return table
@@ -296,7 +515,6 @@ def pdf_articles_total(receipt: Union[Invoice, Estimate], currency: str):
 def pdf_invoice_total(table: Table, receipt:Invoice, currency: str):
     """
     Construit la partie liée aux calcul des totaux pour les factures
-
     """
     #Calcul des Acomptes
     table.add(TableCell(Paragraph("Acomptes", 
@@ -338,8 +556,9 @@ def pdf_estimate_total(table: Table, receipt:Estimate, currency: str):
                     horizontal_alignment=Alignment.RIGHT)))   
     return table
 
-def build_pdf(receipt: Union[Invoice, Estimate], id: int, 
-                                 path: str ="output.pdf", currency: str = "€"):
+def build_pdf(receipt: Union[Invoice, Estimate], id: int,
+    path: str ="output.pdf", color: str = "5f5f5f", currency: str = "€", 
+                   inline: bool = False, prominent_article_table: bool = False):
     """
     Fonction d'assemblage du pdf
     """
@@ -354,14 +573,28 @@ def build_pdf(receipt: Union[Invoice, Estimate], id: int,
                                                                 * Decimal(0.02)
     #Construction de l'entête
     page_layout.add(pdf_header(receipt, id))
+
+    if(prominent_article_table):
+        # Tableaux des articles et des tax
+        page_layout.add(pdf_articles_total(receipt, currency,color))
     # Informations du prestatire et du client
-    tb1, tb2 = pdf_provider_client(receipt)
-    page_layout.add(tb1)
-    page_layout.add(tb2)
-    # Espace pour séparer
-    page_layout.add(Paragraph(" "))
-    # Tableaux des articles et des tax
-    page_layout.add(pdf_articles_total(receipt, currency))
+    if(inline):
+        page_layout.add(Paragraph("Prestataire", font="Helvetica-Bold", 
+                                                        font_size= Decimal(8)))
+        page_layout.add(pdf_provider_inline(receipt))
+        page_layout.add(Paragraph("Client", font="Helvetica-Bold", 
+                                                        font_size= Decimal(8)))
+        page_layout.add(pdf_client_inline(receipt))
+   
+    else:
+        tb1, tb2 = pdf_provider_client(receipt)
+        page_layout.add(tb1)
+        page_layout.add(tb2)
+
+    if(not prominent_article_table):
+        # Tableaux des articles et des tax
+        page_layout.add(pdf_articles_total(receipt, currency, color))
+    
     #On rajoute l'extension pdf si elle n'est pas déjà présente
     if(path[-4:] != ".pdf"):
         path += ".pdf"
@@ -376,7 +609,7 @@ if __name__ == "__main__":
                         "128974654", "Tom", "Pommier", "facturio@gmail.com",
                                                             "logo.jpg")
     client_physique = Client("Lombardo", "Quentin", 
-        "quentin.lombardo@email.com", "HLM Sainte-Muse Toulon", "0678905324")  
+        "quentin.lombardo@email.com", "HLM Sainte-Muse Toulon", "0678905324")
                         
     client_moral = Company("LeRoy",  "LeRoy83@sfr.fr", "12 ZAC de La Crau",
                              "0345678910", "Ben", "Karim","287489404")
@@ -393,4 +626,4 @@ if __name__ == "__main__":
                             note="Invoice de matériel informatiques")
 
     build_pdf(dev, 27, "exemple_devis.pdf")
-    build_pdf(fact, 490, "exemple_facture")
+    build_pdf(fact, 490, "exemple_facture", inline=True, color= "#c44b0e")
