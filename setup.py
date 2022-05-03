03e8098d99e070420c82b18d3fcb067cdd164f11
Plex <thinkplex@riseup.net>
2022-05-03 18:02:11 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='facturio',
+    description='un logiciel de gestion de factures.',
     version='1.0',
+    url='https://github.com/facturio/facturio',
     packages=find_packages(),
-    install_requires=["pygobject", "borb", "geopy"]
+    install_requires=["pygobject", "borb", "geopy"],
+    entry_points={
+        'console_scripts': [
+            'facturio = facturio.main:main'
+        ]
+    },
 )
