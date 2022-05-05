2ae0208d86f884783787e37a84d4efaf7cf1deac
Plex <thinkplex@riseup.net>
2022-05-05 15:56:15 +0200

    display_info

--- facturio/gui/display_info.py
+++ facturio/gui/display_info.py
@@ -1,6 +1,7 @@
 import gi
 gi.require_version("Gtk", "3.0")
-from gui.page_gui import PageGui
+gi.require_version("OsmGpsMap", "1.0")
+from facturio.gui.page_gui import PageGui
 from gi.repository import Gtk, Gdk, Gio, GdkPixbuf, OsmGpsMap
 
 class InfoPerson (PageGui):
