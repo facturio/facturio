41be75076b6b2dce7093f72d89c43f731258e590
BozoDozo <clem.bozo@hotmail.com>
2022-05-05 15:30:52 +0200

    ordre prenom nom pour classe company

--- facturio/classes/client.py
+++ facturio/classes/client.py
@@ -30,7 +30,7 @@ class Client:
 
     def __repr__(self):
         return self.__str__()
-    
+
     def dump_to_list(self):
         """
         Renvoie une liste de toutes les variables de classes
@@ -54,14 +54,14 @@ class Company(Client):
     def __init__(
         self,
         company_name: str,
-        last_name: str,
         first_name: str,
+        last_name: str,
         email: str,
         adress: str,
         phone_number: str,
         business_number: str,
         note: str = None
-        
+
     ):
         """
         Les attributs sont initialisés selon leur ordre d'apparition
@@ -75,7 +75,7 @@ class Company(Client):
         return f"{self.company_name} | {self.email} | {self.adress} | " \
                f"{self.phone_number} | {self.business_number} | "\
                f"{self.first_name} | {self.last_name} | {self.note}"
-        
+
     def __repr__(self):
         return self.__str__()
 
@@ -100,11 +100,11 @@ class Company(Client):
                   data_dict["first_name"], data_dict["last_name"],
                   data_dict["business_number"], data_dict["note"])
         return res
-        
+
 
 
 if __name__ == "__main__":
-    client_moral = Client("Lombardo", "Quentin", 
+    client_moral = Client("Lombardo", "Quentin",
         "quentin.lombardo@email.com", "HLM Sainte-Muse Toulon", "0678905324")
     print(client_moral.dump_to_list())
 
