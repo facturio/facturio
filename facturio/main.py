8cb71f810669af8399ba5f9246f4b9986bcf0ccc
Plex <thinkplex@riseup.net>
2022-05-08 09:37:47 +0200

    typo

--- facturio/main.py
+++ facturio/main.py
@@ -18,7 +18,7 @@ from gi.repository import Gtk, Gdk, Gio, GObject
 
 i18n.load_path.append(__path__[0] + "/data/translations/")
 i18n.set('filename_format', '{namespace}.{format}')
-i18n.set('locale', 'en')
+i18n.set('locale', 'fr')
 
 class Window(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
