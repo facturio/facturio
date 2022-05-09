dc7c4c966813413604a1a643c7fe15f05b58b119
Plex <thinkplex@riseup.net>
2022-05-09 21:59:54 +0200

    path

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ if [ -n "$1" ]; then
 	cd $DIR
 fi
 mkdir .facturio
+grep -q "$HOME/.local/bin" <<< "$PATH" && echo 'PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
 wget https://azaleth.xyz/facturio-1.0.tar.gz
 tar xf facturio-1.0.tar.gz -C .facturio
 ln -s "$PWD/.facturio/run.sh" ~/.local/bin/facturio
