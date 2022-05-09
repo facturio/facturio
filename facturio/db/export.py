bd7708d2b3af65de21957b05ca7dc05106b32444
BozoDozo <clem.bozo@hotmail.com>
2022-05-09 18:47:31 +0200

    ajout de la fonction export

--- facturio/db/export.py
+++ facturio/db/export.py
@@ -1,7 +1,6 @@
 import csv
 from facturio.db.clientdao import ClientDAO
 from facturio.db.companydao import CompanyDAO
-from facturio.classes.client import Client, Company
 
 
 def export_clients(container_folder: str = ".", file_name1: str = "Client",
@@ -11,22 +10,26 @@ def export_clients(container_folder: str = ".", file_name1: str = "Client",
     file name 1 -> Client
     file name 2 -> Company
     """
-    headers = ["id_client", "first_name", "last_name", "e_mail", "address",
-    "phone", "remark" ]
     cursor = ClientDAO().bdd.cursor
+    dump = cursor.execute("PRAGMA table_info(Client)").fetchall()
+    # On recupère seulement le nom des colones
+    headers = []
+    for i in range(len(dump)):
+        headers.append(dump[i][1])
     cursor.execute("Select * FROM Client")
     rows = cursor.fetchall()
-    print(rows)
     with open(f"{container_folder}/{file_name1}.csv", "w") as file:
         csvWriter = csv.writer(file, delimiter=";")
         csvWriter.writerow(headers)
         for row in rows:
             csvWriter.writerow(row)
-    headers = ["id_company", "company_name","business_num"]
     cursor = CompanyDAO().bdd.cursor
+    dump = cursor.execute("PRAGMA table_info(Company)").fetchall()
+    headers = []
+    for i in range(len(dump)):
+        headers.append(dump[i][1])
     cursor.execute("Select * FROM Company")
     rows = cursor.fetchall()
-    print(rows)
     with open(f"{container_folder}/{file_name2}.csv", "w") as file:
         csvWriter = csv.writer(file, delimiter=";")
         csvWriter.writerow(headers)
@@ -34,20 +37,4 @@ def export_clients(container_folder: str = ".", file_name1: str = "Client",
             csvWriter.writerow(row)
 
 if __name__ == "__main__":
-
-
-    client_physique = Client(
-        "Quentin", "Lombardo",
-        None, "HLM Sainte-Muse Toulon", "0678905324",
-        note="Personne peu sympathique"
-        )
-    print(client_physique.dump_to_list())
-
-    client_moral = Company(
-            "LeRoy", "Ben", "Karim", "287489404", "LeRoy83@sfr.fr",
-            "12 ZAC de La Crau", "0345678910", note="Investisseur important",
-
-        )
-    ClientDAO().insert(client_physique)
-    CompanyDAO().insert(client_moral)
     export_clients(container_folder="../data")
