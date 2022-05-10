4a8d1e9cdf0072a8d2df735910c21b485f15f67d
Plex <thinkplex@riseup.net>
2022-05-10 11:05:04 +0200

    logo

--- facturio/db/userdao.py
+++ facturio/db/userdao.py
@@ -28,8 +28,10 @@ class UserDAO:
         # convertir image logo sous forme de fichier binaire
         logo = None
         if user.logo is not None and user.logo != "Aucun":
-            with open(user.logo, "rb") as user.logo:
-                logo = user.logo.read()
+            # print(user.logo)
+            # with open(user.logo, "rb") as lpath:
+            #     logo = lpath.read()
+            logo = user.logo
         values = [logo, user.company_name, user.email, user.address,
                   user.phone_number, user.business_number, user.first_name,
                   user.last_name]
