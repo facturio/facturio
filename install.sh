9c0f0580db19cd9f2d0e2bd80ee5f33f88b25931
Plex <thinkplex@riseup.net>
2022-05-10 11:52:28 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ fi
 
 TAG="v0.9"
 
-rm -fr .facturio "${XDG_DATA_HOME:-$HOME/.local/share}/applications"/facturio.desktop $HOME/.local/bin/facturio
-
 mkdir -p .facturio
 mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}/applications"
+mkdir -p "$HOME"/.local/bin
+
+rm -fr .facturio "${XDG_DATA_HOME:-$HOME/.local/share}/applications"/facturio.desktop $HOME/.local/bin/facturio
 
 echo "$PATH" | grep -q "$HOME/.local/bin" || echo 'PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc
 
@@ -15,5 +16,6 @@ wget https://azaleth.xyz/facturio/${TAG}/facturio-${TAG}.tar.gz
 
 tar xvf facturio-${TAG}.tar.gz -C .facturio
 ln -s "$PWD/.facturio/run.sh" $HOME/.local/bin/facturio
+chmod +x $HOME/.local/bin/facturio
 cp .facturio/facturio.desktop "${XDG_DATA_HOME:-$HOME/.local/share}/applications"
 rm facturio-${TAG}.tar.gz
