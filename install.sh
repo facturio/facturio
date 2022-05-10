b232e9c72c2df550d4bfe3b686c83ffbd332d33f
Plex <thinkplex@riseup.net>
2022-05-10 09:07:38 +0200

    v

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo "$PATH" | grep -q "$HOME/.local/bin" && echo 'PATH=$PATH:$HOME/.local/bin' 
 
 wget https://azaleth.xyz/facturio-1.0.tar.gz
 
-tar xf facturio-1.0.tar.gz -C .facturio
+tar xvf facturio-1.0.tar.gz -C .facturio
 ln -s "$PWD/.facturio/run.sh" ~/.local/bin/facturio
 cp .facturio/facturio.desktop "${XDG_DATA_HOME:-~/.local/share}/applications"
 rm facturio-1.0.tar.gz
